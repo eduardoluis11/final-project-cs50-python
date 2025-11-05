87fa1fc08c08bcc64fa5efb5a7e78d36cd335950
Eduardo Salinas <eduardoluis93@gmail.com>
2025-11-05 22:59:17 +0000

    First Commit
    First Commit. I included a game for testing purposes, which comes from Pygame's official documentation. It just displays a purple screen.

---
+++ test-game-1.py
@@ -0,0 +1,31 @@
+""" Test game used for learning how to create a basic game with Pygame (source: Pygame's official documentation at
+https://www.pygame.org/docs/#tutorials-reference-label).
+
+"""
+# Example file showing a basic pygame "game loop"
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
