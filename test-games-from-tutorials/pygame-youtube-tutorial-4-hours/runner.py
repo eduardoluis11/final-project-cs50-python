7e1b305b5f3525ae847deae06a0ad6ba88cb8594
Eduardo Salinas <eduardoluis93@gmail.com>
2025-11-09 13:13:30 +0000

    Updated test game - Youtube 4 Hour Tutorial
    I changed the name of the game's script to "runner.py", and I added code to the game to add some text that says "My game" in green letters.

---
+++ test-games-from-tutorials/pygame-youtube-tutorial-4-hours/runner.py
@@ -0,0 +1,43 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50)
+
+# This will render the sky
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')   # This renders the ground
+text_surface = test_font.render('My game', False, 'Green')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(sky_surface, (0, 0))   # This renders the sky
+    screen.blit(ground_surface, (0, 300))   # This should render the ground
+    screen.blit(text_surface, (300, 50))
+
+    pygame.display.update()
+    clock.tick(60)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
